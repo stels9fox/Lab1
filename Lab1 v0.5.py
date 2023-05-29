455482842d2639181a480eebca29fe99d2c5f2a1
stels9fox <81447418+stels9fox@users.noreply.github.com>
2023-05-29 22:11:16 +0300

    Добавление решения 1 задачи

--- Lab1 v0.5.py
+++ Lab1 v0.5.py
@@ -1,11 +1,22 @@
-state = 0
-while 1:
+def is_int(char = ''):
+    if len(char) == 1:
+        try:
+            char = int(char)
+            return char
+        except ValueError:
+            print("Это не число")
+    else:
+        print("Это строка длинной минимум 2 символа")
+            
+while True:
     try:
         state = int(input("Выбери номер задачи(1 или 2) или отрицательное число для выхода:"))
     except ValueError:
         print("Это не число")
     if state == 1:
         print("Первое задание")
+        char = is_int(input("Введите символ:"))
+        print(type(char))
     elif state == 2:
         print("Второе задание")
     elif state < 0:
